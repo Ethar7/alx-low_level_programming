1fd7c74c5a3615e56cb830c015429130c35fd541
Ethar7 <nabihethar@yahoo.com>
2023-07-14 19:28:11 -0700

    yt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: function that prints alphapit
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char n = 'a';
+	while (n >= 'a')
+	{
+	putchar(n);
+	a++;
+	}
+	putchar(\n);
+	return (0);
+}
