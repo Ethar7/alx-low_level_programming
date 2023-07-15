a93fc40cc587cef3d53c8d1c6e9cc339d42e5801
Ethar7 <nabihethar@yahoo.com>
2023-07-14 17:50:32 -0700

    iu

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%u is positive\n",n);
+		printf("%u is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	}
 	else
 	{
-		printf("%u is zero\n",n);
+		printf("%u is zero\n", n);
 	}
 	return (0);
 }
