a8e8f9d372c96d9d96b59a08585bcabc20851de0
Ethar7 <nabihethar@yahoo.com>
2023-07-19 14:24:54 -0700

    o

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: function that prints _putchar
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char arr[] = "_putchar";
+
+	for (int i = 0 ; i < 8 ; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
