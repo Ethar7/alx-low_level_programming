d86728bc55d1283739eb53693e150d3757ee7322
Ethar7 <nabihethar@yahoo.com>
2023-07-20 08:33:50 -0700

    uy

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: function that prints lower case character z-a
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+       char x = 'z';
+       while (x >= a)
+       {
+		putchar(x);
+		x++;
+       }
+       putchar('\n');
+       return (0);
+}
