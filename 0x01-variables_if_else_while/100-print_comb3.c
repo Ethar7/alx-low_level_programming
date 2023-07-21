70cab1b3b8d4a8cdbe90cb74f5f62fd26af58d60
Ethar7 <nabihethar@yahoo.com>
2023-07-21 07:25:41 -0700

    iu

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints compinaion of two digits
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 48, y;
+
+	while (x < 58)
+	{
+	y = 48;
+		while (y < 58)
+		{
+		if (x != y && x < y)
+		{
+			putchar(x);
+			putchar(y);
+			if (x + y != 113)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		y++;
+		}
+		x++;
+	}
+	return (0);
+}
