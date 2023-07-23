86cdfeee4ac9b0a58cc07f632ca44cc3cc460593
Ethar7 <nabihethar@yahoo.com>
2023-07-22 22:58:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - checks if number is postive ,negative or 0
+ *
+ * @n: checks input of a function
+ *
+ * Return: return 0,1or -1
+ *
+*/
+int print_sign(int n)
+{
+
+	if (n <= -1)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else if (n == 0)
+	{
+	_putchar(48);
+	return (0);
+	}
+	else if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int print_sign(int n);
 int _isalpha(int c);
 int _putchar(char c);
 int _islower(int c);
