8c2441aea56ad64f7b855300ebbbf66a93ca11b2
Ethar7 <nabihethar@yahoo.com>
2023-07-26 14:44:38 -0700

    o

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,21 +3,21 @@
  * print_to_98 - function that prints numbers equal to 98 ,upper or lower
  *
  * @n: is an input
- * */
+*/
 void print_to_98(int n)
 {
 	int i;
 
 	if (n <= 98)
 	{
-		for ( i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d, ", i);
 		}
 	}
 	else
 	{
-		for ( i = n; i > 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
