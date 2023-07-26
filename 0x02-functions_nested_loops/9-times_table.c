32453170c2a5aba03e9e94a8540c6f03838f6734
Ethar7 <nabihethar@yahoo.com>
2023-07-26 07:53:07 -0700

    i

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@ void times_table(void)
 	int i, j, p, z, l;
 
 	for (i = 0; i <= 9; i++)
-	{     
+	{
 		_putchar(48);
 		for (j = 1; j <= 9; j++)
 		{
 			p = i * j;
-			z = p / 10 ;
-			l = p % 10 ;
+			z = p / 10;
+			l = p % 10;
 			_putchar (',');
 			_putchar (' ');
 			if (p <=9)
@@ -22,11 +22,11 @@ void times_table(void)
  				_putchar(' ');
 				_putchar (p + 48);
 			}
-			else 
+			else
 			{
 			_putchar(z + 48);
 			_putchar(l + 48);
-		}	
+		}
 	}
 	_putchar('\n');
 	}
