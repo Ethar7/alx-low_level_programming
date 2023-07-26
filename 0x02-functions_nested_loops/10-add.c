e73a18547070a3dce4e3a23e71aaf0c33a0952fb
Ethar7 <nabihethar@yahoo.com>
2023-07-26 14:00:16 -0700

    o

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - this function adding two numbers
+ *
+ * @a: is avariable
+ * @b: is another variable
+ *
+ * Return: returns sum
+*/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
