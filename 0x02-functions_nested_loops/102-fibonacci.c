fa9a95357265f81c5efd8df5db53ac02654fb724
Ethar7 <nabihethar@yahoo.com>
2023-08-05 15:01:00 -0700

    i

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - Entery point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *
+ * Return: Always return 0
+*/
+int main(void)
+{
+	int yf, i, x1 = 0, x2 = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		yf = x1 + x2;
+		printf("%u", yf);
+		x1 = x2;
+		x2 = yf;
+		if (i == 49)
+		{
+		printf("\n");
+		}
+		else
+		{
+		printf(", ");
+		}
+	}
+	return (0);
+}
