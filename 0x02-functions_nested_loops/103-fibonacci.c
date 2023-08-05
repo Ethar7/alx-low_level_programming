d545c665122c4825ff775fc7b117023db09bf10b
Ethar7 <nabihethar@yahoo.com>
2023-08-05 15:34:09 -0700

    i

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - Entery point
+ *
+ * Description: prints the even fibomicial numbers
+ *
+ * Return: Always return 0
+*/
+int main(void)
+{
+	unsigned long yf, i, x1 = 0, x2 = 1;
+	float s;
+
+	while (true)
+	{
+		yf = x1 + x2;
+		if (yf > 4000000)
+		{
+			break;
+		}
+		else if (yf % 2 ==0)
+		{
+			s += yf;
+			x1 = x2;
+			x2 = yf;
+		}
+	}
+	printf ("%.0f\n", s);
+	return (0);
+}
