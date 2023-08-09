646129b2fccb92812ee33e9f9805fef3c4b15ac5
Ethar7 <nabihethar@yahoo.com>
2023-08-09 01:49:59 -0700

    p

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * main - Entery point
+ *
+ * Description: function that prints fizz or fizz buzz ,buzz
+ *
+ * Return: Always 0 success
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
+
