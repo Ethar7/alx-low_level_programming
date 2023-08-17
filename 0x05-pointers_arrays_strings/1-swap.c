7fac01aae351ca7dda5b9f8f33370f4a3647ad8f
Ethar7 <nabihethar@yahoo.com>
2023-08-17 11:01:23 -0700

    u

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap - function that swap two numbers
+ *
+ * @a: is an input
+ * @b: is an input
+*/
+void swap_int(int *a, int *b)
+{
+	int z;
+
+	z = a*;
+	a* = b*;
+	b* = a;
+}
