e2b016b97d3800ce0653b709bf54611579f725b6
Ethar7 <nabihethar@yahoo.com>
2023-08-18 10:15:58 -0700

    i

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,19 @@
 /**
  * rev_string - reverse a string
  *
- * @s: is a pointer  that points on astring
- *
+ * @s: is a pointer  that points on a string
 */
+void rev_string(char *s)
+{
+	int i, l = 0;
+	char x;
+
+	while (s[l] != '\0')
+		++l;
+	for (i = 1; i <= l / 2; i++)
+	{
+		x = s[i];
+		s[i] = s[l - i];
+		s[l - i] = x;
+	}
+}
