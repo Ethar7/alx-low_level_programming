231333d0faa0037d4046985b3be8335ef9cbb033
Ethar7 <nabihethar@yahoo.com>
2023-08-18 01:22:02 -0700

    i

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - function that prints string
+ *
+ * @str: is an input pointer
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 48);
+		*str++;
+	}
+	_putchar('\n');
+}
