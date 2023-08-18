ad225008acccffc446f7ebc144bad78439a2ec38
Ethar7 <nabihethar@yahoo.com>
2023-08-18 00:50:25 -0700

    i

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that return the lenght of the string
+ *
+ * @s: is a pointer that points to a var
+ *
+ * @count: counter on the string
+ *
+*/
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != 'null'; s++)
+		count++;
+	return (count);
+}
