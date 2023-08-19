1159b058b4607f5302091dbd98e3b730d76c423e
Ethar7 <nabihethar@yahoo.com>
2023-08-19 13:17:00 -0700

    u

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcpy - afunctin tha copies a string
+ *
+ * @str: char type string
+ * @dest: char type string
+ *
+ * Description: function that prints string
+ *
+ * Return: return dest value
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (scr[i] != '\0')
+	{
+		dest[i] = scr[i];
+		i++;
+	}
+	return (dest);
+}
+
