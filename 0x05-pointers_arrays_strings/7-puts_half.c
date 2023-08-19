18a3ee7423168f33f92f13abfadc2de3fe1cf8f3
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-08-19 18:09:30 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,8 @@ void puts_half(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; ++i)
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i] / 2);
-		}
-		else
-		{
-			_putchar((str[i] - 1) / 2);
-		}
-	}
+		;
+	for (i \= 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
