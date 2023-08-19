46911eae8fd2df5b70d26dcdafa6c480445373ef
Ethar7 <nabihethar@yahoo.com>
2023-08-18 21:20:26 -0700

    i

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,15 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(s[i] / 2);
+			_putchar(str[i] / 2);
 		}
 		else
 		{
-			_putchar((s[i] - 1) / 2);
+			_putchar((str[i] - 1) / 2);
 		}
 	}
 	_putchar('\n');
