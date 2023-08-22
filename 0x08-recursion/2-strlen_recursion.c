eb01230b778f76478beb2b11eddb9a103352d748
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-08-22 11:21:18 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,9 +10,9 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s != '\0')
+	if (*s > '\0')
 	{
-		i += _strlen_recursions(s) + 1;
+		i += _strlen_recursions(s + 1) + 1;
 	}
-	return (i);
+return (i);
 }
