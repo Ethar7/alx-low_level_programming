4aa81db0aa91908bd722ce597c1fa4fb58ab4fc8
Ethar7 <nabihethar@yahoo.com>
2023-08-22 05:00:31 -0700

    i

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - this function returns the power of two numbers
+ *
+ * @x: is an input base
+ * @y: y is the power
+ *
+ * Return: the power value
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return(-1);
+	else if (y == 0)
+		return(1);
+	else if (y == 1)
+		return(x);
+	return(x * _pow_recursion(x, y - 1));
+}
