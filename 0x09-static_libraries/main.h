6951b25a580e26f82cb514469a7ca79b387a0bf6
Ethar7 <nabihethar@yahoo.com>
2023-08-25 04:13:06 -0700

    i

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <ctype.h>
+
+
+int _abs(int);
+int _isalpha(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int _islower(int c);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+
+#endif
