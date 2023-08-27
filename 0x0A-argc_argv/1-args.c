bed819a61217f9633edd151bb8e20d0504d145a8
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-08-27 12:38:18 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  *
  * Return: 0;
 */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] _attribute_((unused)))
 {
-	printf("%d \n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
