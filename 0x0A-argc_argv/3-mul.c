71aaa51ea9c09531e9542378595e5a68354a1d04
Ethar7 <nabihethar@yahoo.com>
2023-08-27 09:47:52 -0700

    o

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,11 +15,12 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		x = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n",sum);
+		printf("%d\n", x);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
