10eee5fc7019344032521203a6358d177a73f0c0
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-08-30 02:30:07 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,7 +25,7 @@ void _puts(char *str)
 */
 int _atoi(const char *s)
 {
-	int sign = 1;
+int sign = 1;
 	unsigned long int resp = 0, firstnum, i;
 
 	for (firstnum = 0; !(s[firstnum] >= 48 && s[firstnum] <= 57); firstnum++)
