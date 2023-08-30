b65f400ec404e3198967995fef366a10fd1c6beb
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-08-30 10:52:10 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 /**
  * get_nodeint_at_index - returns node
- *
  * @head: pointer to first node
  * @index: index of node
  *
@@ -14,5 +13,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (node = head, n = 0; node && n < index; node = node->next, n++)
 		;
-		return (node);
+	return (node);
 }
