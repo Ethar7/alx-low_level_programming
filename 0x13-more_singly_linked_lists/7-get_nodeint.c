253bb48035f9707e2b0a7801c3ea09785873e2d4
Ethar7 <nabihethar@yahoo.com>
2023-08-29 19:22:29 -0700

    o

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to first node
  * @index: index of node
  *
- * Retern: pointer of node
+ * Return: pointer of node
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
