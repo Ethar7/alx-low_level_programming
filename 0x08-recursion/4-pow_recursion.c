1e28e740b936fd653f6564fb83e73a452f65c568
Ethar7 <nabihethar@yahoo.com>
2023-09-28 19:46:42 -0700

    o

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,9 +6,9 @@
  * @x: input
  * @y: input
  *
- * Return: int 
+ * Return: int
  *
- * */
+*/
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -17,5 +17,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else if (y == 1)
 		return (x);
-	return (x * _pow_recursion(x , y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
