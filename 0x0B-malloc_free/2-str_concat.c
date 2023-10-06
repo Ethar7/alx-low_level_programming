e3b99bcc0338a2992251516ce61f16d21615eef0
Ethar7 <nabihethar@yahoo.com>
2023-10-05 19:09:34 -0700

    o

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * string_length - string length
+ *
+ * @s: string
+ *
+ * Return: size
+*/
+int string_length(char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+
+/**
+ * str_concat - concatenate two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: size of two strings
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, i = 0;
+	char *m;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+
+		return (NULL);
+	}
+	size1 = string_length(s1);
+	size2 = string_length(s2);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i <= size1 + size2; i++)
+		{
+			if (i < size1)
+			{
+				m[i] = s1[i];
+			}
+			else
+			{
+				m[i] = s2[i - size1]
+			}
+		}
+	}
+	return (m);
+}
