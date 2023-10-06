a44d6644f856400ef4cee03c53cc96b2a3145e0b
Ethar7 <nabihethar@yahoo.com>
2023-10-05 17:45:58 -0700

    p

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * _strdup - function that prints a copy 
+ *
+ * @str: string
+ *
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *m;
+
+	for (i = 0; str[size] != '\0'; size++)
+		;
+	m = malloc(size * sizeof(*str) + 1);
+	if (str == NULL || m == 0)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
