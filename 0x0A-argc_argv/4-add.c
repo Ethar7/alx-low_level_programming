d01fe6f978e359e01885bd26e8acc26ebfa127e2
Ethar7 <nabihethar@yahoo.com>
2023-10-07 23:26:05 -0700

    i

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ *
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		while (c = argv[argc])
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+		c++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
