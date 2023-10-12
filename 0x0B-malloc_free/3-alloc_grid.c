1537d74dcfad33280a02f4e4b7fd5d02a717dc1c
Ethar7 <nabihethar@yahoo.com>
2023-10-12 02:24:37 -0700

    O

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, *ptr;
+	int i, j, **ptr;
 
 	ptr = malloc(sizeof(*ptr) * height);
 	if (width <= 0 || height <= 0 || ptr == 0)
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 			free(ptr);
 			return (NULL);
 			for (j = 0; j < width; j++)
-				ptr[i][j];
+				ptr[i][j] = 0;
 		}
 	}
 	return (ptr);
