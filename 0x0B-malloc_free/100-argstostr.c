f9e7efc97248dea2ea30724ff0a36536018fd483
Ethar7 <nabihethar@yahoo.com>
2023-10-12 07:39:32 -0700

    p

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * string_len - string size
+ *
+ * @s: pointer
+ *
+ * Return: size
+*/
+int string_len(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * argstostr - function that concatenates all the arguments
+ *
+ * @ac: int
+ * @av: arguments
+ *
+ * Return: pointer
+*/
+char *argstostr(int ac, char **av)
+{
+	int x ,y, z, l;
+	char *ptr;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
+
+	for (; x < ac; x++, y++)
+		y += string_len(av[x]);
+	ptr = malloc(sizeof(char) * y + 1);
+
+	if (ptr = 0)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (z = 0; av[i][z] != '\0'; z++, l++)
+			ptr[l] = av[x][z];
+		ptr[l] = '\n';
+	}
+	ptr[l] = '\0';
+	return(ptr);
+}
