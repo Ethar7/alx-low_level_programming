d9f450fa49720ef3ae26f23e8a0414f38603f370
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-10-13 13:26:53 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	len = max - min + 1;
-	ptr = malloc(sizeof(*ptr) * len);
+	ptr = malloc(sizeof(int) * len);
 	if (!ptr)
 		return (NULL);
 	for (i = 0; i < len; i++)
