06ae653b85fd4cabb0c5c9b381544cfba831ed56
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-10-16 06:13:23 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -66,7 +66,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	new->owner = malloc(sizeof(char) * (string_length(owner) + 1));
 
-	if (new->owner)
+	if (new->owner == NULL)
 	{
 		free(new->name);
 		free(new);
