025447955e6240568a27ccd78e298de9369c75e4
Ethar7 <nabihethar@yahoo.com>
2023-10-19 17:38:17 -0700

    o

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node to the start of the list 
+ * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
- * 
+ *
  * Return: size of list
 */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head = malloc(sizeof(list_t));
 
