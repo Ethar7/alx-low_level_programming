3031631eac2de44ccdd70227e7e1b09aee077fa6
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-10-23 04:50:04 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = array + size - 1;
+	//int *end = array + size - 1;
 
-	if (array && size && action)
+	if (array && action)
 		while (array <= end)
 			action(*array++);
 }
