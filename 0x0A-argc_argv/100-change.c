eede789a83480ef30d8006a5bbb7c6727237cdea
Ethar7 <nabihethar@yahoo.com>
2023-10-23 18:12:14 -0700

    o

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print least number of cents
+ * @argc: number of argument
+ * @argv: array of arguments
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, least_money = 0, money = atoi(argv[1]);
+	int unlimeted_cents[] = {25, 10, 5, 2, 1};
+
+	if (argc == 2)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (money <= unlimeted_cents[i])
+			{
+				least_money += money / unlimeted_cents[i];
+				money % unlimeted_cents[i];
+
+				if (money % unlimeted_cents == 0)
+					break;
+			}
+		}
+		printf("%d\n", least_money);
+	}
+	else
+	{
+		printf("Error\n", return (1));
+	}
+	return (0);
+}
+	
