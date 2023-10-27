697abb8906e062e698643f24830778c5899fd67e
Ethar7 <nabihethar@yahoo.com>
2023-10-26 20:08:25 -0700

    p

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flib_bits - return num of bits
+ *
+ * @n: first
+ * @m: second
+ *
+ * Return: num
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xorval = n ^ m;
+	unsigned int count = 0;
+
+	while (xoral)
+	{
+		if (xoral & lul)
+			count++;
+		xoral = xooral >> 1;
+	}
+	return (count);
+}
