c9fc9008c59ab573c2108a5cd28aaa5ae6b13406
Ethar7 <nabihethar@yahoo.com>
2023-11-03 01:00:32 -0700

    o

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Entery point
+ * 
+ * Description: print fabonancci numbers to 98
+ *
+ * Return: fabonancci
+ *
+*/
+int main(void)
+{
+	int c;
+	unsigned long fb1 = 0, fb2 = 1, sum = 0;
+
+	for (c = 0; c < 98; c++)
+	{
+	sum = fb1 + fb2;
+	printf("%lu", sum);
+	fb1 = fb2;
+	fb2 = sum;
+	if (c == 97)
+		printf("\n");
+	else 
+		printf(",");
+	}
+	return (0);
+}
