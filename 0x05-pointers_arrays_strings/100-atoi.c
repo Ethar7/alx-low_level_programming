dd9f899924728cdfc7a43497207b47a2226befcf
Ethar7 <nabihethar@yahoo.com>
2024-03-05 19:49:58 -0800

    this is pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,12 @@ int _atoi(char *s)
         unsigned int num = 0;
         int sign = 1;
 
-        while (*s != NULL)
+        while (*s != 0)
         {
                 if (*s == '-')
                         sign *= -1;
                 else if (*s >= '0' && *s <= '9')
                         num = num * 10 + *s - 48;
-		*s++;
 	}
 	return (sign * num);
 }
