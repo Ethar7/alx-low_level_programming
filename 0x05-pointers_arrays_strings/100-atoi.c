65462e9b47464cd994bab21890b29443db794bd9
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-03-06 21:00:35 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,12 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	int sign = 1;
 
-	while (*s++)
+	do
 	{
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + *s - 48;
-	}
+	}while (*s++);
 	return (sign * num);
 }
