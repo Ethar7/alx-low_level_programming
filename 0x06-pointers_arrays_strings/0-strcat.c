8ed80074ffbb9b0c21e009d9fd6a89b72142e9e2
Ethar7 <nabihethar@yahoo.com>
2024-03-13 06:28:15 -0700

    o

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - function concat two strings
+ *
+ * @src: main string
+ * @dest: concatenated string
+ *
+ * Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int c1 = 0, c2 = 0;
+
+	while (dest[c1])
+		c1++;
+	while (src[c2])
+	{
+		dest[c1++] = src[c2];
+		c2++;
+	}
+	return (dest)
+}
