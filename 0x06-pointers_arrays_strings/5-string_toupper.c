aa57fe4ba3d90488809f48562c5e5e28d4435622
Ethar7 <nabihethar@yahoo.com>
2024-03-14 01:14:22 -0700

    p

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - function that return string low to up
+ *
+ * @string: string
+ * 
+ * Return: upper
+*/
+char *string_toupper(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 97 && string[i] <= 122)
+			string[i] = string[i] - 32;
+	}
+}
