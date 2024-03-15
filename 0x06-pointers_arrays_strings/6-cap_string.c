c29cc0dc1a179770f50ef8213183683ec05ee6a1
Ethar7 <nabihethar@yahoo.com>
2024-03-15 02:38:06 -0700

    o

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * *cap_string - function return capital string
+ *
+ * @ptr: pointer to char
+ *
+ * Return: ptr
+*/
+char *cap_string(char *ptr)
+{
+	int del = 1;
+
+	while (*ptr)
+	{
+		if (is_delm(*ptr)
+			del = 1;
+		else if (is_lower(*ptr) && del)
+		{
+			*ptr -= 32;
+			del = 0;
+		}
+		else 
+			del = 0;
+		ptr++;
+	}
+	return (ptr);
+}
+/**
+ * is_lower - return lower case char
+ *
+ * @c: var
+ *
+ * Return: c
+*/
+int is_lower(char c)
+{
+	if (c >= 97 && c <= 122)
+		return (c);
+}
+/**
+ * is_delm - function checks del
+ *
+ * @c: var
+ *
+ * Return: c
+*/
+int is_delm(char c)
+{
+	int i;
+	char delmi[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delmi[i])
+			return (1);
+		return (0);
+}
