94b57cbca48a25750bca94e6deeb03788e7adbbe
Ethar7 <nabihethar@yahoo.com>
2024-03-16 02:40:38 -0700

    o

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *leet - function that encodes a string
+ *
+ * @a: var
+ *
+ * Return: a
+*/
+char *leet(char *a)
+{
+	int i;
+	char arr[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	char *ap = a;
+
+	while (*a)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*a == arr[i] || *a == arr[i] + 32)
+			{
+				*a = value[i] + 48;
+			}
+			a++;
+		}
+	}
+	return (ap);
+}
