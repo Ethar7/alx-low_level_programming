60e7298d522aafa283e962e2cb1026e5413d88a1
Ethar7 <nabihethar@yahoo.com>
2024-03-21 22:33:52 -0700

    i

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strspn - function calc the length in bytes 
+ * _strspn - function calc the length in bytes
  *
  * @s: pointer to char
  * @accept: pointer
@@ -11,14 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);
 		}
-		
+
 	}
 	return (i);
 }
