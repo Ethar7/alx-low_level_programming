72350862d86574a043ff984474d9ef44b4af0237
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-03-22 08:03:30 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,12 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 	char *p;
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
