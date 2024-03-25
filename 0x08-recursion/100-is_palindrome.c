1748b17a87e3d7bbc429fd4079ec7d10d0d3f2eb
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-03-25 16:33:22 +0200

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,10 +27,8 @@ int compare(char *s, int n1, int n2)
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
-		{
 			return (1);
-		}
-		return (0 + compare(s, n1 + 1, n2 - 1);
+		return (0 + compare(s, n1 + 1, n2 - 1));
 	}
 return (0);
 }
@@ -44,8 +42,6 @@ return (0);
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
-	{
 		return (1);
-	}
-return (compare(s, 0, _strlen_recursion(s) - 1);
+return (compare(s, 0, _strlen_recursion(s) - 1));
 }
