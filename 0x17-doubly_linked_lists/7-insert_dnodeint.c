a2a9943587462af2f721c86e3c8937af5d6dbea5
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-09 14:22:06 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -36,11 +36,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (current->prev != NULL)
 			current->prev->next = new_node;
 		current->prev = new_node;
-		return new_node;
+		return (new_node);
 	}
 	else if (current == NULL && i == idx)
 	{
-		return add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 	}
 	else
 	{
