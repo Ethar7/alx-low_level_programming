808f3516d5f4f4050697f0d33bb7f3d46ce93e48
Ethar7 <nabihethar@yahoo.com>
2024-05-09 04:18:22 -0700

    o

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,39 +7,44 @@
  * @n: The data for the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
- */
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  dlistint_t *new_node, *current;
-  unsigned int i;
+	dlistint_t *new_node, *current;
+	unsigned int i;
 
-  if (h == NULL)
-    return (NULL);
+	if (h == NULL)
+		return (NULL);
 
-    if (idx == 0)
-        return add_dnodeint(h, n);
+	if (idx == 0)
+		return add_dnodeint(h, n);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    current = *h;
+	new_node->n = n;
+	current = *h;
 
-    for (i = 0; current != NULL && i < idx; i++)
-        current = current->next;
+	for (i = 0; current != NULL && i < idx; i++)
+		current = current->next;
 
-    if (i == idx) { // Insert at the index
-        new_node->next = current;
-        new_node->prev = current->prev;
-        if (current->prev != NULL)
-            current->prev->next = new_node;
-        current->prev = new_node;
-        return new_node;
-    } else if (current == NULL && i == idx) { // Insert at the end
-        return add_dnodeint_end(h, n);
-    } else { // Index is out of range
-        free(new_node);
-        return (NULL);
-    }
+	if (i == idx) 
+	{
+		new_node->next = current;
+		new_node->prev = current->prev;
+		if (current->prev != NULL)
+			current->prev->next = new_node;
+		current->prev = new_node;
+		return new_node;
+	} 
+	else if (current == NULL && i == idx) 
+	{
+		return add_dnodeint_end(h, n);
+	} 
+	else 
+	{
+		free(new_node);
+		return (NULL);
+	}
 }
