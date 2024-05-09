15b3d504ebb4d00d9f4f8086854d69236dabf21a
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-09 12:54:45 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	while (current->next != NULL)
@@ -31,6 +31,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	current->next = new_node;
 	new_node->prev = current;
-	return new_node;
+	return (new_node);
 }
 
