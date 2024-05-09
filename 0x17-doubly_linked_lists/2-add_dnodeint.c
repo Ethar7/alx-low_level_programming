39a0f46cc3af4dea3f9527166774a14693450b82
Ethar7 <nabihethar@yahoo.com>
2024-05-09 01:46:28 -0700

    o

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,16 +6,17 @@
  *
  * Return: The address of the new element, or NULL
 */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL) 
+	if (new_node == NULL)
 	{
-		return (NULL); 
+		return (NULL);
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
 
-	if (*head != NULL) 
+	if (*head != NULL)
 	{
 		new_node->next = *head;
 		(*head)->prev = new_node;
